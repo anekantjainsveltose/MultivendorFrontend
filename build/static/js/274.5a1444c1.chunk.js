7ec009e4e984bf85d4cc3f3afac08232a78fe0a6
Anekant <Anekantjainsagar@gmail.com>
2023-06-23 15:38:38 +0530

    Adddedd build

---
+++ build/static/js/274.5a1444c1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[274],{2257:function(e,t,n){"use strict";n.r(t);var a=n(11),r=n(12),c=n(14),u=n(13),i=n(1),o=n.n(i),s=function(e){Object(c.a)(n,e);var t=Object(u.a)(n);function n(){return Object(a.a)(this,n),t.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return o.a.createElement("div",null," textInComponent ")}}]),n}(i.Component);t.default=s}}]);
+//# sourceMappingURL=274.5a1444c1.chunk.js.map
